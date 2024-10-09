9e07a5bddad6ba4b70d706beed69798def4b57d6
zan <137709585+rfq-zan@users.noreply.github.com>
2024-10-09 17:22:51 +0700

    Add files via upload

---
+++ QUIZ_23-185_MOHAMMAD RIFQI RAZZAN PRATAMA/src/quiz_23/pkg185_mohammad/rifqi/razzan/pratama/QUIZ_23185_MOHAMMADRIFQIRAZZANPRATAMA.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package quiz_23.pkg185_mohammad.rifqi.razzan.pratama;
+
+/**
+ *
+ * @author ZAN
+ */
+public class QUIZ_23185_MOHAMMADRIFQIRAZZANPRATAMA {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
